44da5b28e9eef17f572f9fd40e9744dd67e08ea6
chadkope73-blip <chadkope73@gmail.com>
2026-03-13 00:00:47 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,35 @@
-const CACHE_NAME = "skywalker247-cache-v1";
+const CACHE_NAME = "skywalker247-v1";
 
 const urlsToCache = [
 "/",
 "/index.html",
 "/logo.png",
 "/topo-bg.jpeg",
-"/app.js"
+"/app.js",
+"/sky-data.json",
+"/manifest.json"
 ];
 
 self.addEventListener("install", event => {
 
 event.waitUntil(
 caches.open(CACHE_NAME)
-.then(cache => cache.addAll(urlsToCache))
+.then(cache => {
+return cache.addAll(urlsToCache);
+})
+);
+
+});
+
+self.addEventListener("activate", event => {
+
+event.waitUntil(
+caches.keys().then(keys => {
+return Promise.all(
+keys.filter(key => key !== CACHE_NAME)
+.map(key => caches.delete(key))
+);
+})
 );
 
 });
@@ -20,12 +37,17 @@ caches.open(CACHE_NAME)
 self.addEventListener("fetch", event => {
 
 event.respondWith(
-caches.match(event.request)
-.then(response => {
 
-return response || fetch(event.request);
+caches.match(event.request).then(response => {
+
+if(response){
+return response;
+}
+
+return fetch(event.request);
 
 })
+
 );
 
 });
